11675bbf1baa1c8dc4d779acb45023be72471469
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-08 02:41:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,6 +9,22 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-
+    Show.minimum(:rating)
   end
+
+  def self.least_popular_show
+    Show.lowest_rating
+  end 
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end 
+
+  def self.popular_shows
+    Show.where("rating > ?",5)
+  end 
+
+  def self.shows_by_alphabetical_order
+    Show.order(:name)
+  end 
 end
